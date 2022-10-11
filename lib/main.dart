8853fcc95704bb857c028aaa5a9f03e0fd195634
Anoop Jose <anoop@flutterly.co.uk>
2022-10-11 22:39:57 +0100

    hmm

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Hmm'),
+      home: const MyHomePage(title: 'Hmmmmm'),
     );
   }
 }
